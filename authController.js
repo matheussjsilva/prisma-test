626d00aa619c56963fc3f68981599c5c6ca29a85
Matheus Silva <matheus.silva.contato26@gmail.com>
2024-12-12 01:20:21 -0300

    adds authController file

---
+++ authController.js
@@ -0,0 +1,23 @@
+import { generateToken } from "./auth.js";
+
+export default class AuthController {
+  constructor(prisma) {
+    this.prisma = prisma;
+  }
+  async login(request, response) {
+    const { email } = request.body;
+    try {
+      const user = await prisma.user.findUnique({
+        where: { email },
+      });
+      if (!user) {
+        return response.status(404).json({ error: "Usuário não encontrado" });
+      }
+      const token = generateToken(user);
+      return response.status(200).json({ token });
+    } catch (error) {
+      console.error("Erro ao autenticar usuário: ", error);
+      return response.status(500).json({ error: "Erro interno do servidor" });
+    }
+  }
+}
